a3e988dbb5c0f81d08999cdde50289f8ac909659
Camila Prado <50000125+milaepg@users.noreply.github.com>
2022-09-06 18:48:08 -0300

    hola mundo

---
+++ Hola mundo/hola_mundo.py
@@ -0,0 +1,21 @@
+print("Hola, mundo")
+
+nombre = "Camila"
+print("Hola,", nombre) 
+
+nombre = "Camila"
+print("Hola, " + nombre)
+
+numero = 25 
+print("Hola,", numero)
+
+numero = "25" 
+print("Hola, "+ numero) 
+
+comida_uno = "pizza"
+comida_dos = "hamburguesa"
+print("Amo comer {} y {}.".format(comida_uno, comida_dos)) 
+
+comida_uno = "pizza"
+comida_dos = "hamburguesa"
+print(f"Amo comer {comida_uno} y {comida_dos}.") 
